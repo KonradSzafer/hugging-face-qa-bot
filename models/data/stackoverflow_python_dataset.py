f8734a1b26bd6a0ac29690ce08d47b2a9bfc6f02
KonradSzafer <szafer.konrad@gmail.com>
2023-02-25 19:42:58 +0100

    removed html

--- models/data/stackoverflow_python_dataset.py
+++ models/data/stackoverflow_python_dataset.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from datasets import load_dataset
+from bs4 import BeautifulSoup
 
 
 def preprocess_dataset():
@@ -10,9 +11,24 @@ def preprocess_dataset():
             example['answer_score'] > 5 and
             datetime.strptime(example['answer_date'], '%Y-%m-%dT%H:%M:%SZ').year > 2010
         )
+
+    def html2text(example):
+        soup = BeautifulSoup(example, 'html.parser')
+        return ''.join(soup.findAll(string=True))
+
+    def transform_questions(example):
+        example['question_body'] = html2text(example['question_body'])
+        return example
+
+    def transform_answers(example):
+        example['answer_body'] = html2text(example['answer_body'])
+        return example
+
+    dataset = dataset.map(lambda example: transform_questions(example))
+    dataset = dataset.map(lambda example: transform_answers(example))
     dataset = dataset.remove_columns([
         'question_score', 'question_date', 'question_id',
-        'answer_date', 'answer_id', 'answer_score', 'tags'
+        'answer_date', 'answer_id', 'answer_score', 'tags',
     ])
     return dataset
 
