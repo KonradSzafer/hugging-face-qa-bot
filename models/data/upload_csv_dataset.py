55d1a3603e5f8c4addfbd3c440b215a41df03a85
KonradSzafer <szafer.konrad@gmail.com>
2023-02-27 23:12:39 +0100

    dataset split in custom datasets

--- models/data/upload_csv_dataset.py
+++ models/data/upload_csv_dataset.py
@@ -1,11 +1,24 @@
 import sys
 import pandas as pd
 from datasets import Dataset, DatasetDict
+from sklearn.model_selection import train_test_split
 
 
-if __name__ == '__main__':
+
+def main():
     dataset_name = sys.argv[1]
+    test_size = float(sys.argv[2]) if len(sys.argv) > 2 else 0.1
+    print(f'dataset: {dataset_name}, test size: {test_size}')
+
     filename = f'datasets/{dataset_name}.csv'
     df = pd.read_csv(filename)
     dataset = Dataset.from_pandas(df)
-    dataset.push_to_hub(f'KonradSzafer/{dataset_name}', private=False)
+    train_dataset, test_dataset = train_test_split(dataset, test_size=test_size)
+    train_dataset = Dataset.from_dict(train_dataset)
+    test_dataset = Dataset.from_dict(test_dataset)
+    dataset_dict = DatasetDict({'train': train_dataset, 'test': test_dataset})
+    dataset_dict.push_to_hub(f'KonradSzafer/{dataset_name}', private=False)
+
+
+if __name__ == '__main__':
+    main()
