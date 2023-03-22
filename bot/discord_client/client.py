53e99847ec1a22242e8f1bbd891782bc77012879
KonradSzafer <szafer.konrad@gmail.com>
2023-03-22 15:44:03 +0100

    added usernames in chat context

--- bot/discord_client/client.py
+++ bot/discord_client/client.py
@@ -4,17 +4,29 @@ from bot.logger import logger
 
 
 class DiscordClient(discord.Client):
-    def __init__(self, model):
+    def __init__(
+        self,
+        model,
+        num_last_messages: int = 5,
+        use_names_in_context: bool = True
+    ):
         logger.info('Initializing Discord client...')
         intents = discord.Intents.all()
         intents.message_content = True
         super().__init__(intents=intents, command_prefix='!')
         self.model = model
-        self.n_last_messages: int = 5
-        self.initial_prompt: str = 'previous conversation messages:'
+        self.num_last_messages: int = num_last_messages
+        self.use_names_in_context: bool = use_names_in_context
+
+    def _set_initial_prompt(self) -> None:
+        name = str(self.user).split('#')[0]
+        self.initial_prompt: str = \
+            f'your name is: {name}\n' \
+            f'previous conversation messages:'
 
     async def on_ready(self):
         logger.info('Successfully logged in as: {0.user}'.format(self))
+        self._set_initial_prompt()
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -22,8 +34,11 @@ class DiscordClient(discord.Client):
 
         last_messages: List[str] = []
         async for msg in message.channel.history(
-            limit=self.n_last_messages):
-            last_messages.append(msg.content)
+            limit=self.num_last_messages):
+            if self.use_names_in_context:
+                last_messages.append(f'{msg.author}: {msg.content}')
+            else:
+                last_messages.append(msg.content)
         last_messages.reverse()
         last_messages.pop() # remove last message from context
         context = self.initial_prompt + '\n'
