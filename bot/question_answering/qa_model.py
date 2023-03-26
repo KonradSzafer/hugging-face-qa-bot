9841b2815c2ad63d9ef92a8e4f0b108676863e34
JanekDev <m.wilinski11@gmail.com>
2023-03-27 01:50:29 +0200

    Added index to response context

--- bot/question_answering/qa_model.py
+++ bot/question_answering/qa_model.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 from langchain import PromptTemplate, HuggingFaceHub, LLMChain
+from langchain.embeddings import HuggingFaceHubEmbeddings
+from langchain.llms.base import LLM
+from llama_index import GPTSimpleVectorIndex, LangchainEmbedding, LLMPredictor
+from typing import Mapping, Optional, List
 
 
 class Model(ABC):
@@ -31,15 +35,23 @@ class LangChainModel(Model):
             }, 
             huggingfacehub_api_token=hf_api_key
         )
+        embedding_model = HuggingFaceHubEmbeddings(repo_id="sentence-transformers/all-MiniLM-L6-v2", huggingfacehub_api_token=hf_api_key)
         self.template = 'Question: {question} \nContext: {context}'
         self.prompt = PromptTemplate(
             template=self.template,
             input_variables=['question', 'context']
         )
         self.llm_chain = LLMChain(prompt=self.prompt, llm=self.model)
+        self.knowledge_index = GPTSimpleVectorIndex.load_from_disk(
+            "index.json",
+            embed_model=LangchainEmbedding(embedding_model),
+            llm_predictor=LLMPredictor(self.model)
+        )
 
 
     def get_answer(self, context: str, question: str) -> str:
+        doc_extracted = self.knowledge_index.query(question).response
+        context += doc_extracted
         return self.llm_chain.run(
             question=question,
             context=context
