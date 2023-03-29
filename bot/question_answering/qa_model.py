9ec7d4df4551d9e67721a97026da6056cddd39d9
KonradSzafer <szafer.konrad@gmail.com>
2023-03-29 23:16:24 +0200

    local qa model

--- bot/question_answering/qa_model.py
+++ bot/question_answering/qa_model.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
+from typing import Mapping, Optional, List, Any
+import torch
+from transformers import pipeline
 from langchain import PromptTemplate, HuggingFaceHub, LLMChain
 from langchain.embeddings import HuggingFaceHubEmbeddings
 from langchain.llms.base import LLM
 from llama_index import GPTSimpleVectorIndex, LangchainEmbedding, LLMPredictor
-from typing import Mapping, Optional, List
 
 
 class Model(ABC):
@@ -15,27 +17,55 @@ class Model(ABC):
         pass
 
 
+class CustomLLM(LLM):
+    def __init__(self, model_name: str):
+        self.pipeline = pipeline(
+            "text-generation",
+            model=model_name,
+            device=0,
+            model_kwargs={"torch_dtype":torch.bfloat16}
+        )
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        prompt_length = len(prompt)
+        response = self.pipeline(prompt, max_new_tokens=num_output)[0]["generated_text"]
+        return response[prompt_length:]
+
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        return {"name_of_model": "empty"}
+
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+
+
 class LangChainModel(Model):
     def __init__(
         self,
         hf_api_key: str,
         question_answering_model_id: str,
         embedding_model_id: str,
+        run_localy: bool = False
     ):
         super().__init__()
-        self.model = HuggingFaceHub(
-            repo_id=question_answering_model_id,
-            model_kwargs={
-                'max_length': 1000,
-                'length_penalty': 2,
-                'num_beams': 16,
-                'no_repeat_ngram_size': 2,
-                'temperature': 0.8,
-                'top_k': 256,
-                'top_p': 0.8
-            }, 
-            huggingfacehub_api_token=hf_api_key
-        )
+        if run_localy:
+            self.model = CustomLLM(question_answering_model_id)
+        else:
+            self.model = HuggingFaceHub(
+                repo_id=question_answering_model_id,
+                model_kwargs={
+                    'max_length': 1000,
+                    'length_penalty': 2,
+                    'num_beams': 16,
+                    'no_repeat_ngram_size': 2,
+                    'temperature': 0.8,
+                    'top_k': 256,
+                    'top_p': 0.8
+                }, 
+                huggingfacehub_api_token=hf_api_key
+            )
+
         embedding_model = HuggingFaceHubEmbeddings(
             repo_id=embedding_model_id,
             huggingfacehub_api_token=hf_api_key
