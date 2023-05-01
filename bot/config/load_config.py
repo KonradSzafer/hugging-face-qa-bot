00f730cf86d90ae3f2b5be8125b76ac8a1080e13
KonradSzafer <szafer.konrad@gmail.com>
2023-05-01 13:21:26 +0200

    env.example update

--- bot/config/load_config.py
+++ bot/config/load_config.py
@@ -25,16 +25,16 @@ def to_bool(variable: str) -> bool:
 @dataclass
 class Config:
     huggingface_token: str = get_env('HUGGINGFACEHUB_API_TOKEN')
+    discord_token: str = get_env('DISCORD_TOKEN')
     question_answering_model_id: str = get_env('QUESTION_ANSWERING_MODEL_ID')
     embedding_model_id: str = get_env('EMBEDDING_MODEL_ID')
     index_name: str = get_env('INDEX_NAME')
-    run_locally: bool = to_bool(get_env('RUN_LOCALLY', True))
-    discord_token: str = get_env('DISCORD_TOKEN')
     use_docs_in_context: bool = to_bool(get_env('USE_DOCS_IN_CONTEXT', True))
     use_messages_in_context: bool = to_bool(get_env('USE_MESSEGES_IN_CONTEXT', True))
     num_last_messages: int = int(get_env('NUM_LAST_MESSAGES', 2))
     use_names_in_context: bool = to_bool(get_env('USE_NAMES_IN_CONTEXT', False))
     enable_commands: bool = to_bool(get_env('ENABLE_COMMANDS', True))
+    run_locally: bool = to_bool(get_env('RUN_LOCALLY', True))
 
     def asdict(self) -> Dict:
         return asdict(self)
