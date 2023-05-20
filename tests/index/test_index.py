876cc32e2d02b22b728863d74ba160084919ed55
KonradSzafer <szafer.konrad@gmail.com>
2023-05-20 22:14:31 +0200

    finished index tests

--- tests/index/test_index.py
+++ tests/index/test_index.py
@@ -1,38 +1,39 @@
 import pytest
+from typing import Any
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 
-
-def get_embedding_model():
+@pytest.fixture(scope="module")
+def embedding_model() -> HuggingFaceInstructEmbeddings:
     model_name = "hkunlp/instructor-large"
     embed_instruction = "Represent the Hugging Face library documentation"
     query_instruction = "Query the most relevant piece of information from the Hugging Face documentation"
-    embedding_model = HuggingFaceInstructEmbeddings(
+    return HuggingFaceInstructEmbeddings(
         model_name=model_name,
         embed_instruction=embed_instruction,
         query_instruction=query_instruction,
     )
-    return embedding_model
 
+@pytest.fixture(scope="module")
+def index_path() -> str:
+    return "index/"
 
-INDEX_PATH = "index/"
-EMBEDDING_MODEL = get_embedding_model()
+@pytest.fixture(scope="module")
+def index(embedding_model: HuggingFaceInstructEmbeddings, index_path: str):
+    return FAISS.load_local(index_path, embedding_model)
 
+@pytest.fixture(scope="module")
+def query() -> str:
+    return "How to use the tokenizer?"
 
-def test_load_index():
-    index = FAISS.load_local(INDEX_PATH, EMBEDDING_MODEL)
-    assert index is not None
+def test_load_index(embedding_model: HuggingFaceInstructEmbeddings, index_path: str):
+    index = FAISS.load_local(index_path, embedding_model)
+    assert index is not None, "Failed to load index"
 
+def test_index_page_content(index, query: str):
+    query_docs = index.similarity_search(query=query, k=3)
+    assert isinstance(query_docs[0].page_content, str)
 
-def test_index_page_content():
-    index = FAISS.load_local(INDEX_PATH, EMBEDDING_MODEL)
-    docs = index.similarity_search(
-        query="How to use the tokenizer?", k=3)
-    assert isinstance(docs[0].page_content, str)
-
-
-def test_index_metadata():
-    index = FAISS.load_local(INDEX_PATH, EMBEDDING_MODEL)
-    docs = index.similarity_search(
-        query="How to use the tokenizer?", k=3)
-    assert isinstance(docs[0].metadata['source'], str)
+def test_index_metadata(index, query):
+    query_docs = index.similarity_search(query=query, k=3)
+    assert isinstance(query_docs[0].metadata['source'], str)
