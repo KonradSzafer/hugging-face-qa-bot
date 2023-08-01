6c5efbfffdca594fc2de728a3b60d65ccdd8da78
KonradSzafer <szafer.konrad@gmail.com>
2023-08-01 14:17:27 -0400

    repo names script update

--- data/get_hugging_face_repositories.py
+++ data/get_hugging_face_repositories.py
@@ -4,19 +4,28 @@ import requests
 from typing import List
 
 
-def get_repositories_names(token):
-    url = f'https://api.github.com/orgs/huggingface/repos?per_page=1000'
-    headers = {'Authorization': f'token {token}'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        repos = json.loads(response.content)
-        repo_names = [
-            repo['full_name'] for repo in repos
-            if repo['stargazers_count'] >= 100
-        ]
-        return repo_names
-    else:
-        return 'Error: '+str(response.status_code)
+def get_repositories_names(token: str, min_stars: int) -> List[str]:
+    repos_per_page = 100
+    repo_names = []
+    i = 0
+    while True:
+        url = \
+            f'https://api.github.com/orgs/huggingface/repos?' \
+            f'per_page={repos_per_page}&page={i}'
+        headers = {'Authorization': f'token {token}'}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            repos = json.loads(response.content)
+            repo_names += [
+                repo['full_name'] for repo in repos
+                if repo['stargazers_count'] >= min_stars
+            ]
+            if len(repos) < repos_per_page:
+                break
+            i += 1
+        else:
+            return 'Error: '+str(response.status_code)
+    return list(set(repo_names))
 
 
 def save_repositories_urls(repositories_names: List[str], output_filename: str):
@@ -29,6 +38,11 @@ def save_repositories_urls(repositories_names: List[str], output_filename: str):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--token', type=str)
+    parser.add_argument('--stars', type=str)
     args = parser.parse_args()
-    repositories = get_repositories_names(token=args.token)
+    repositories = get_repositories_names(token=args.token, min_stars=int(args.stars))
+    repositories += [
+        'huggingface/hf-endpoints-documentation',
+        'gradio-app/gradio'
+    ]
     save_repositories_urls(repositories, 'datasets/hf_repositories_urls_scraped.json')
