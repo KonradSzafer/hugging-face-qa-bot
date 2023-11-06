30f1cd99e3cdba5a4aa8ce2a1cec8ba367f85561
KonradSzafer <szafer.konrad@gmail.com>
2023-11-06 15:49:23 -0500

    docker cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,20 @@
-FROM ubuntu:latest
+FROM debian:bullseye-slim
 
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get -y update && \
     apt-get -y upgrade && \
-    apt-get -y install git python3.10 python3-pip
+    apt-get -y install git python3.11 python3-pip
 
 COPY requirements.txt .
 RUN pip install --upgrade pip && \
     pip install --no-cache-dir -r requirements.txt
 
 WORKDIR /hugging-face-qa-bot
-COPY config/api/ config/api/
-COPY api/ api/
+COPY . .
 
+RUN ls -la
 EXPOSE 8000
 
-ENTRYPOINT [ "python3", "-m", "api" ]
+ENTRYPOINT [ "python3", "-m", "api" ] # to run the api module
+# ENTRYPOINT [ "python3", "-m", "discord_bot" ] # to host the bot
--- qa_engine/mocks.py
+++ qa_engine/mocks.py
@@ -6,36 +6,22 @@ from langchain.llms.base import LLM
 
 class MockLocalBinaryModel(LLM):
     """
-    Mock Local Binary Model class, used for generating the string "a".
-
-    Args:
-        model_id (str): The ID of the model to be mocked.
-
-    Attributes:
-        model_path (str): The path to the model to be mocked.
-        llm (str): The string "a".
-
-    Raises:
-        ValueError: If the model_path does not exist.
+    Mock Local Binary Model class.
     """
 
     model_path: str = None
-    llm: str = 'READY TO MOCK'
+    llm: str = 'Mocked Response'
 
-    def __init__(self, model_id: str = None):
+    def __init__(self):
         super().__init__()
-        self.model_path = f'bot/question_answering/{model_id}'
-        if not os.path.exists(self.model_path):
-            raise ValueError(f'{self.model_path} does not exist')
-
 
     def _call(self, prompt: str, stop: Optional[list[str]] = None) -> str:
         return self.llm
 
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
-        return {'name_of_model': self.model_path}
+        return {'name_of_model': 'mock'}
 
     @property
     def _llm_type(self) -> str:
-        return self.model_path
+        return 'mock'
